62da89fd6b66302b12b9d4cc348f17bc7cb25d02
liguikang <liguikang.gz.chain@outlook.com>
2025-08-10 19:36:18 +0800

    add：后端项目初始化

---
+++ src/main/java/com/lgk/lgkaicodeservice/LgkAiCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lgk.lgkaicodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgkAiCodeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LgkAiCodeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgk/lgkaicodeservice/LgkAiCodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgk.lgkaicodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgkAiCodeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
