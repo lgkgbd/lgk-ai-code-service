ab9222350fdf7fc25c83d4a407fe2d7bb09a2432
liguikang <liguikang.gz.chain@outlook.com>
2025-08-11 21:14:14 +0800

    add：openapi2ts、axios（请求的代码），删除没用的代码

---
+++ lgk-ai-code-service-frontend/openapi2ts.config.ts
@@ -0,0 +1,5 @@
+﻿export default {
+  requestLibPath: "import request from '@/request'",
+  schemaPath: 'http://localhost:8123/api/v3/api-docs',
+  serversPath: './src',
+}
--- lgk-ai-code-service-frontend/src/App.vue
+++ lgk-ai-code-service-frontend/src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import BasicLayout from '@/layouts/BasicLayout.vue'
+
+import { healthCheck } from '@/api/healthController.ts'
+
+healthCheck().then((res) => {
+  console.log(res)
+})
+
 </script>
 
 <template>
   <BasicLayout />
-  
+
 </template>
 
 <style scoped>
