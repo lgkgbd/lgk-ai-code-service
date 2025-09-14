8b7295fe09789ca08f9e24d070f11c83afaff399
liguikang <843702876@qq.com>
2025-09-14 08:12:35 +0800

    feat：上传文件功能（限制只能管理员用，免得被刷）

---
+++ src/test/java/com/lgk/lgkaicodeservice/manager/CosManagerTest.java
@@ -0,0 +1,15 @@
+package com.lgk.lgkaicodeservice.manager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CosManagerTest {
+
+    @Test
+    void uploadFile() {
+
+
+
+    }
+}
